9dc4b69f90757e03bb69a50cbeab75f136a23d7a
Xiaoshan Wang <xswang.k@gmail.com>
2025-11-12 23:39:46 +0800

    delete

--- cmd_record.sh
+++
@@ -1,7 +0,0 @@
-python -m http.server
-
-git add latest_data.json
-git commit -m "update"
-git config pull.rebase false
-git pull
-git push
--- update.sh
+++
@@ -1,8 +0,0 @@
-# 1. 添加更改
-git add latest_data.json
-
-# 2. 记录更改 (m = message, 消息)
-git commit -m "Update data for $(date +%Y-%m-%d)"
-
-# 3. 推送!
-git push
